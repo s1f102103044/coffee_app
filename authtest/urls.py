50bb5848d1f9420d7b7901222b6c568fa60d8e66
s1f102103044 <85671690+s1f102103044@users.noreply.github.com>
2022-12-22 18:46:23 +0900

    サイトの動きの設定は完了しました。
    後はHTMLとCSSをお願いします。

--- authtest/urls.py
+++ authtest/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('priv', views.private_page, name='priv'),
+    path('priv1', views.private_page1, name='priv1'),
+    path('priv2', views.private_page2, name='priv2'),
+    path('priv3', views.private_page3, name='priv3'),
     path('pub', views.public_page, name='pub'),
 ]
--- authtest/views.py
+++ authtest/views.py
@@ -6,8 +6,16 @@ def home(request):
     return render(request, 'authtest/home.html', {})
 
 @login_required
-def private_page(request):
-    return render(request, 'authtest/private.html', {})
+def private_page1(request):
+    return render(request, 'authtest/private1.html', {})
+
+@login_required
+def private_page2(request):
+    return render(request, 'authtest/private2.html', {}) 
+
+@login_required
+def private_page3(request):
+    return render(request, 'authtest/private3.html', {})     
 
 def public_page(request):
     return render(request, 'authtest/public.html', {})
