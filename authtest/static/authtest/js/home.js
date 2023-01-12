f42341111fb63b3c9473f71c406ec294e2f46521
s1f102103044 <85671690+s1f102103044@users.noreply.github.com>
2023-01-12 16:35:41 +0900

    完成

---
+++ authtest/static/authtest/js/home.js
@@ -0,0 +1,27 @@
+// <戻るボタンの動作>
+
+jQuery(function() {
+    var appear = false;
+    var pagetop = $('#page_top');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //50pxスクロールしたら
+        if (appear == false) {
+          appear = true;
+          pagetop.stop().animate({
+            'bottom': '50px' //下から50pxの位置に
+          }, 300); //0.3秒かけて現れる
+        }
+      } else {
+        if (appear) {
+          appear = false;
+          pagetop.stop().animate({
+            'bottom': '-50x' //下から-50pxの位置に
+          }, 300); //0.3秒かけて隠れる
+        }
+      }
+    });
+    pagetop.click(function () {
+      $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+      return false;
+    });
+  });
